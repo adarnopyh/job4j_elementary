61d236baf048c6d1658812c8cb7e46b96bac9528
adarnopyh <askolds.darnopihs@gmail.com>
2023-10-28 13:31:17 +0300

    check

--- src/main/java/ru/job4j/ExercisesCheckOne.java
+++ src/main/java/ru/job4j/ExercisesCheckOne.java
@@ -2,14 +2,18 @@ package ru.job4j;
 
 public class ExercisesCheckOne {
     public static void main(String[] args) {
-        // 1 выражение
+        /*
+          1 выражение
+         */
         long l = 129;
         byte b1 = (byte) l;
         System.out.println("1 выражение");
         System.out.println(l);
         System.out.println(b1);
 
-        // 2 выражение
+        /*
+         * 2 выражение
+         */
         float f = 12358.7f;
         int i = 45981;
         short rsl = (short) (f + i);
@@ -17,21 +21,27 @@ public class ExercisesCheckOne {
         System.out.println(i);
         System.out.println(rsl);
 
-        // 3 выражение
+        /*
+         * 3 выражение
+         */
         char c = 45000;
         float i2 = c;
         System.out.println("3 выражение");
         System.out.println(c);
         System.out.println(i2);
 
-        // 4 выражение
+        /*
+         * 4 выражение
+         */
         System.out.println("4 выражение");
         double d = 121.19;
         byte b = (byte) d;
         System.out.println(d);
         System.out.println(b);
 
-        // 5 выражение
+        /*
+         * 5 выражение
+         */
         short s = 1500;
         char c2 = (char) s;
         double d2 = c2;
