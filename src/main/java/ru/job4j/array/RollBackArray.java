e8d5ddb72e370d8ef3507ebe825cbe2ddeef4945
adarnopyh <askolds.darnopihs@gmail.com>
2023-11-13 00:10:47 +0200

    6.4.2. Обход массива с последнего элемента [#361619 #459467]

--- src/main/java/ru/job4j/array/RollBackArray.java
+++ src/main/java/ru/job4j/array/RollBackArray.java
@@ -8,5 +8,4 @@ public class RollBackArray {
         }
         return result;
     }
-
 }
--- src/test/java/ru/job4j/array/RollBackArrayTest.java
+++ src/test/java/ru/job4j/array/RollBackArrayTest.java
@@ -35,5 +35,4 @@ public class RollBackArrayTest {
         int[] result = RollBackArray.rollback(input);
         assertThat(result).containsExactly(expected);
     }
-
 }
