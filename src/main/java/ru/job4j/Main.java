1620a65491985b1d184a30ac078e6619be3d54ef
adarnopyh <askolds.darnopihs@gmail.com>
2023-12-11 22:42:28 +0200

    test

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        boolean check = 2 > 0 && 3 < 5 && !false;
+        boolean check = true;
         System.out.println(check);
     }
 }
