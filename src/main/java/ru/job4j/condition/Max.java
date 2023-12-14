04e12c55851c8ac3643eb9126df1f28fa6730b45
adarnopyh <askolds.darnopihs@gmail.com>
2023-12-14 12:26:27 +0200

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,7 +1,32 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left > right ? left : right;
+    public Max(int one, int two) {
+    }
+
+    public Max(int one, int two, int three) {
+    }
+
+    public Max(int one, int two, int three, int four) {
+    }
+
+    public int max(int one, int two) {
+        return one > two ? one : two;
+    }
+
+    public int max(int one, int two, int three) {
+        return max(
+                three,
+                max(one, two)
+        );
+    }
+
+    public int max(int one, int two, int three, int four) {
+        return max(
+                four,
+                max(
+                    three,
+                    max(one, two)
+        ));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -1,24 +1,50 @@
 package ru.job4j.condition;
 
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MaxTest {
     @Test
-    void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+    public void whenMax1To2Then2() {
+        int one = 1;
+        int two = 2;
+        Max maximus = new Max(one, two);
+        int result = maximus.max(one, two);
         int expected = 2;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenMax2To2ThenAny() {
-        int left = 2;
-        int right = 2;
-        int result = Max.max(left, right);
+    public void whenMax2To2ThenAny() {
+        int one = 2;
+        int two = 2;
+        Max maximus = new Max(one, two);
+        int result = maximus.max(one, two);
         int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax2To3To5Then5() {
+        int one = 2;
+        int two = 3;
+        int three = 5;
+        Max maximus = new Max(one, two, three);
+        int result = maximus.max(one, two, three);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenMax2To3To5To7Then7() {
+        int one = 2;
+        int two = 3;
+        int three = 5;
+        int four = 7;
+        Max maximus = new Max(one, two, three, four);
+        int result = maximus.max(one, two, three, four);
+        int expected = 7;
         assertThat(result).isEqualTo(expected);
     }
 }
